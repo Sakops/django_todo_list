51c10ce6bdc83b6e585876d32f8f43c161e71b7d
sakops <sakops@pop-os.localdomain>
2022-05-23 12:14:49 +0600

    second

--- settings/settings.py
+++ settings/settings.py
@@ -124,10 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+
+STATIC_ROOT = Path(BASE_DIR / 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'static'),
+    Path(BASE_DIR / 'static'),
 ]
 
 # Default primary key field type
